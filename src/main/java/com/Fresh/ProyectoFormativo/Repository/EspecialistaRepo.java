fcf4942f6918accda0825842426ca300c21669c0
moisesph <mois.mp8@gmail.com>
2023-05-28 17:23:50 -0500

    Trabajar en nueva rama

--- src/main/java/com/Fresh/ProyectoFormativo/Repository/EspecialistaRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Fresh.ProyectoFormativo.Repository;
-
-import com.Fresh.ProyectoFormativo.Documents.Especialista;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EspecialistaRepo extends MongoRepository<Especialista, String> {
-}
