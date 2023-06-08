3ab9f3923b9664a5056b67d3684e98ffe99bfb28
Maite Martinez <salomeydavid507@gmail.com>
2023-06-08 02:39:33 -0500

    Agregando cambios

--- src/main/java/com/Fresh/ProyectoFormativo/Entity/Codigo_administrador.java
+++ src/main/java/com/Fresh/ProyectoFormativo/Entity/Codigo_administrador.java
@@ -4,8 +4,13 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "codigo_administrador")
 public class Codigo_administrador {
-    @Column(name = "codigo", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "codigo")
     private String codigo;
 }
