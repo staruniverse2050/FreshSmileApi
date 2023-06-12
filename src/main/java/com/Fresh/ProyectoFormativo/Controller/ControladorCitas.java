b915539fb03cb75c0ce26eb19fb1912ffa2cf5b7
Maite Martinez <salomeydavid507@gmail.com>
2023-06-12 02:01:58 -0500

    Configurando lo mismo :)

--- src/main/java/com/Fresh/ProyectoFormativo/Controller/ControladorCitas.java
+++ src/main/java/com/Fresh/ProyectoFormativo/Controller/ControladorCitas.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/FreshSmile")
@@ -44,10 +45,11 @@ public class ControladorCitas {
     @PutMapping("/ModificarCita/{id}")
     public ResponseEntity<Citas> modificarCita(@PathVariable int id, @RequestBody Citas citas) {
         Citas citaExistente = citasService.BuscarCita(id);
-        citas.setNumero_documento(citas.getNumero_documento());
-        Citas citaActualizada = citasService.ModificarCita(citas);
+        citaExistente.setNumero_documento(citas.getNumero_documento());
+        Citas citaActualizada = citasService.ModificarCita(citaExistente);
         return ResponseEntity.ok(citaActualizada);
     }
+
 
 
     @DeleteMapping("CancelarCita/{id}")
